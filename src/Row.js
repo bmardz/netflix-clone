9cdd73a0daa63eaa4bcad58caa3e323d7d499929
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-09-09 20:25:37 +1000

    updated files

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row() {
-    return (
-        <div>
-            
-        </div>
-    )
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on specific conditions
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* row_poster */}
+
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/*container -> posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
 const API_KEY = "2321f15b6c949349fc5fc49dfa2ef5dc";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&langauge=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&langauge=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-}
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&langauge=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&langauge=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
 export default requests;
